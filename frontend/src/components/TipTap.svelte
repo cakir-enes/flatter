3e85eeb336ac4fd6c62fe649434a99cf1e4fe172
cakir-enes <cakir-enes@outlook.com>
2021-05-05 01:06:32 +0300

    pos format

--- frontend/src/components/TipTap.svelte
+++ frontend/src/components/TipTap.svelte
@@ -1,115 +1,129 @@
 <script lang="ts">
-    import { Editor, posToDOMRect } from "@tiptap/core";
-    import RefFinder from "./RefFinder.svelte";
-    import { tiptapEditor } from "../actions";
+  import { Editor, posToDOMRect } from "@tiptap/core";
+  import RefFinder from "./RefFinder.svelte";
+  import { tiptapEditor } from "../actions";
 
-    export let show: boolean = false;
+  export let show: boolean = false;
 
-    let element;
-    let editor: Editor;
-    let showRef = false;
-    let refRect: ClientRect = {
-        left: 0,
-        top: 0,
-        width: 0,
-        bottom: 0,
-        height: 0,
-        right: 0,
+  let element;
+  let editor: Editor;
+  let showRef = false;
+  let refRect: ClientRect = {
+    left: 0,
+    top: 0,
+    width: 0,
+    bottom: 0,
+    height: 0,
+    right: 0,
+  };
+
+  function position() {
+    const { state, composing } = editor.view;
+    const { doc, selection } = state;
+
+    const { from, to, empty, $anchor } = selection;
+    let rect = posToDOMRect(editor.view, from, to);
+    const { left, width, y } = editor.view.dom.getBoundingClientRect();
+
+    refRect = {
+      left: left - 7,
+      top: rect.top + rect.height,
+      width: width,
+      bottom: 0,
+      height: window.innerHeight - y,
+      right: 0,
     };
+  }
 
-    function position() {
-        const { state, composing } = editor.view;
-        const { doc, selection } = state;
-
-        const { from, to, empty, $anchor } = selection;
-        let rect = posToDOMRect(editor.view, from, to);
-        const { left, width, y } = editor.view.dom.getBoundingClientRect();
-
-        refRect = {
-            left: 180,
-            top: rect.top + rect.height,
-            width: width,
-            bottom: 0,
-            height: window.innerHeight - y,
-            right: 0,
-        };
+  $: {
+    if (show && !showRef) {
+      editor.chain().focus();
     }
-
-    $: {
-        if (show && !showRef) {
-            editor.chain().focus();
-        }
-        if (!show) {
-            showRef = false;
-        }
+    if (!show) {
+      showRef = false;
     }
+  }
 
-    function handleKeyDown(e: KeyboardEvent) {
-        if (e.ctrlKey) {
-            e.stopPropagation();
-            if (e.key === " " && show) {
-                position();
-                showRef = !showRef;
-            }
-        } else {
-            showRef = false;
-        }
+  function handleKeyDown(e: KeyboardEvent) {
+    if (e.ctrlKey) {
+      e.stopPropagation();
+      if (e.key === " " && show) {
+        position();
+        showRef = !showRef;
+      }
+    } else {
+      console.log(e);
+
+      showRef = false;
     }
+  }
 </script>
 
-<RefFinder rect={refRect} show={showRef} />
+<div class="container" class:show>
+  <RefFinder rect={refRect} show={showRef} />
 
-<div
+  <div
     class="editor"
     class:show
     on:keydown={handleKeyDown}
     use:tiptapEditor={{
-        init: (e) => {
-            editor = e;
-        },
+      init: (e) => {
+        editor = e;
+      },
     }}
->
+  >
     {#if editor}
-        <button
-            on:click={() =>
-                editor.chain().focus().toggleHeading({ level: 1 }).run()}
-            class:active={editor.isActive("heading", { level: 1 })}
-        >
-            H1
-        </button>
-        <button
-            on:click={() =>
-                editor.chain().focus().toggleHeading({ level: 2 }).run()}
-            class:active={editor.isActive("heading", { level: 2 })}
-        >
-            H2
-        </button>
-        <button
-            on:click={() => editor.chain().focus().setParagraph().run()}
-            class:active={editor.isActive("paragraph")}
-        >
-            P
-        </button>
+      <button
+        on:click={() =>
+          editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        class:active={editor.isActive("heading", { level: 1 })}
+      >
+        H1
+      </button>
+      <button
+        on:click={() =>
+          editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        class:active={editor.isActive("heading", { level: 2 })}
+      >
+        H2
+      </button>
+      <button
+        on:click={() => editor.chain().focus().setParagraph().run()}
+        class:active={editor.isActive("paragraph")}
+      >
+        P
+      </button>
     {/if}
     <div class="editorx" bind:this={element} />
+  </div>
 </div>
 
 <style lang="scss">
-    .editor {
-        width: 100%;
-        max-width: 80ch;
-        z-index: -1;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        max-height: 90%;
-        background: #023;
-        opacity: 0;
-    }
+  .container {
+    top: 0;
+    left: 0;
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(20, 133, 189, 0.363);
+    opacity: 0;
+    z-index: -1;
+  }
 
-    .show {
-        z-index: 10;
-        opacity: 1;
-    }
+  .editor {
+    width: 100%;
+    max-width: 80ch;
+    z-index: -1;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 10%;
+    max-height: 90%;
+    background: #023;
+    opacity: 0;
+  }
+
+  .show {
+    z-index: 10;
+    opacity: 1;
+  }
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,11 +3,11 @@ import App from './App.svelte';
 import * as Wails from '@wailsapp/runtime';
 
 let app;
+app = new App({
+	target: document.body,
+});
+// Wails.Init(() => {
 
-Wails.Init(() => {
-	app = new App({
-		target: document.body,
-	});
-}); 
+// });
 
 export default app;
